35972279c6c67ed9f9790b39834b0bc324f324a8
Jon Johnson <j.johnson@ioe.ac.uk>
2015-10-14 10:18:00 +0100

    Create fandr5.py
    Add label to CodeListScheme

---
+++ fandr5.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# Add label to CodeListScheme
+# replace a string with another
+# Usage: fandr.py "old" "new" file
+
+import sys
+import re
+import glob
+
+rpstring = "<ddi:CodeList "
+psreplace1="  <r:Label>"+"\n"+"          <r:Content xml:lang=\"en-GB\">"
+psreplace2="</r:Content>"+"\n"+"        </r:Label>"+"\n"+"  "+str(rpstring)
+
+renaming = open("renaming_list.txt",'r')
+newdict={}
+for line in renaming:
+	short,description,floc,fpub=line.split('\t')
+	newdict[short.strip()]=description.strip()
+
+
+for filename in glob.glob('*.ddi32.rp.xml'):
+  fn=filename.split(".")
+  instname=str(newdict[fn[0]])+" Codelist"
+  psreplacement=psreplace1+instname+psreplace2  
+  try:
+    s = open(filename).read()
+  except IOError:
+    print "No such file"
+    sys.exit(2)
+  # Well, we've got this far - the file must exist!
+  if rpstring in s:
+    s=s.replace(rpstring,psreplacement,1)
+    
+    f=open(filename, "w")
+    f.write(s)
+    f.flush()
+    f.close()
+      
+  else:
+    print "No occurences of "+ str(rpstring) +" found.in "+str(filename)
