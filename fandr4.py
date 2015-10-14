1cc3e4a1adabd47fb3259236fd95824ff5899b38
Jon Johnson <j.johnson@ioe.ac.uk>
2015-10-14 10:16:43 +0100

    Create fandr4.py
    Adds LogicalProductName

---
+++ fandr4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Adds LogicalProductName
+# replace a string with another
+# Usage: fandr.py "old" "new" file
+
+import sys
+import re
+import glob
+
+rpstring = "<ddi:DataRelationship "
+psreplace1="  <r:Label xmlns=\"ddi:reusable:3_2\">"+"\n"+"      <r:Content xml:lang=\"en-GB\">"
+psreplace2="</r:Content>"+"\n"+"  </r:Label>"+"\n"+"  "+str(rpstring)
+
+renaming = open("renaming_list.txt",'r')
+newdict={}
+for line in renaming:
+	short,description,floc,fpub=line.split('\t')
+	newdict[short.strip()]=description.strip()
+
+
+for filename in glob.glob('*.ddi32.rp.xml'):
+  fn=filename.split(".")
+  instname=str(newdict[fn[0]])+" Logical Product"  
+  psreplacement=psreplace1+instname+psreplace2  
+  try:
+    s = open(filename).read()
+  except IOError:
+    print "No such file"
+    sys.exit(2)
+  
+  # Well, we've got this far - the file must exist!
+  if rpstring in s:
+    s=s.replace(rpstring,psreplacement)
+    
+    f=open(filename, "w")
+    f.write(s)
+    f.flush()
+    f.close()
+      
+  else:
+    print "No occurences of "+ str(rpstring) +" found.in "+str(filename)
